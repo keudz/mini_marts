ceae8e8198cac6df7de11d9e380d9a1878d8cf16
nguyenthedat <dnguyenthe706@gmail.com>
2026-03-30 22:12:51 +0700

    add @valid

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -4,6 +4,7 @@ import com.example.demo.constant.UrlConstant;
 import com.example.demo.dto.response.ProductResponseDTO;
 import com.example.demo.entity.Product;
 import com.example.demo.service.AdminService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -20,13 +21,13 @@ public class AdminController {
         @Autowired
     private AdminService blockUser;
     @PostMapping(UrlConstant.API_V1_BLOCK_USER)
-  public void BlockUser(@RequestParam int id) {
+  public void BlockUser(@RequestParam @Valid int id) {
         blockUser.blockUser(id);
     }
     @Autowired
     private AdminService unblockUser;
     @PostMapping(UrlConstant.UNLOCK_USER)
-    public void unLockUser(@RequestParam int id) {
+    public void unLockUser(@RequestParam @Valid int id) {
         unblockUser.unlockUser(id);
     }
 
